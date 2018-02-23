c187736a0ee73903ba8a5ca581509830142e1eeb
Leonid Khelemes <lkhelemes@intellias.com>
2018-02-23 14:43:28 +0200

    week1

--- week1/task3/solution.py
+++ week1/task3/solution.py
@@ -3,15 +3,15 @@ a = int(sys.argv[1])
 b = int(sys.argv[2])
 c = int(sys.argv[3])
 
-discr = b**2 - 4 * a * c;
+discr = b**2 - 4 * a * c
 print("Дискриминант D = %.2f" % discr)
 if discr > 0:
-	import math
-	x1 = (-b + math.sqrt(discr)) / (2 * a)
-	x2 = (-b - math.sqrt(discr)) / (2 * a)
-	print("x1 = %.2f \nx2 = %.2f" % (x1, x2))
+    import math
+    x1 = (-b + math.sqrt(discr)) / (2 * a)
+    x2 = (-b - math.sqrt(discr)) / (2 * a)
+    print("x1 = %.2f \nx2 = %.2f" % (x1, x2))
 elif discr == 0:
-	x = -b / (2 * a)
-	print("x = %.2f" % x)
+    x = -b / (2 * a)
+    print("x = %.2f" % x)
 else:
-	print("Корней нет")
+    print("Корней нет")
